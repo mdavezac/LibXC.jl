f55c48bfe47a8e9ad15d65268fc513ec409eab22
Mayeul d'Avezac <m.davezac@ucl.ac.uk>
2017-02-12 21:24:30 +0000

    A few more functionals

--- src/LibXC.jl
+++ src/LibXC.jl
@@ -8,15 +8,17 @@ end
 
 function _get_version()
     version = Cint[0, 0, 0]
-    ccall((:xc_version, libxc), Void, (Ptr{Cint}, Ptr{Cint}, Ptr{Cint}), Ref(version),
+    ccall((:xc_version, libxc), Void, (Ref{Cint}, Ref{Cint}, Ref{Cint}), Ref(version),
           Ref(version, 2), Ref(version, 3))
     VersionNumber(version...)
 end
 
-const version = _get_version()
+const lib_version = _get_version()
 
 _xc_key(name::AbstractString) = ccall((:xc_functional_get_number), Cint, (Cstring,), name)
-_xc_name(key::Integer) = ccall((:xc_functional_get_number), Cstring, (Cint,), key)
-
+function _xc_name(key::Integer)
+    name = ccall((:xc_functional_get_name), Cstring, (Cint,), key)
+    unsafe_string(name)
+end
 
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -4,7 +4,10 @@ using Base.Test
 
 @testset "> Internal API" begin
     @testset ">> xc key from name" begin
-        @test LibXC.version == v"3.0.0"
+        @test LibXC.lib_version == v"3.0.0"
+        @test LibXC._xc_key("LDA_X") == 1
+        @test LibXC._xc_key("lda_x") == 1
+        @test LibXC._xc_name(1) == "lda_x"
     end
 end
 end
