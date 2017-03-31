f2933f1c52abe359f271cc15d11f2c774329a454
Mayeul d'Avezac <mdavezac@gmail.com>
2017-03-31 19:38:29 +0100

    Parameterized derivative types

--- src/units.jl
+++ src/units.jl
@@ -4,9 +4,11 @@ using UnitfulHartree
 typealias CReal Union{Cfloat, Cdouble}
 typealias Density{T <: CReal} Quantity{T, typeof(dimension(u"𝖊*a₀^-3")), typeof(u"𝖊*a₀^-3")}
 typealias EnergyDensity{T <: CReal} Quantity{T, typeof(dimension(u"Eₕ/𝖊")), typeof(u"Eₕ/𝖊")}
-typealias PotentialDensity{T <: CReal}
-    Quantity{T, typeof(dimension(u"Eₕ*a₀^3/𝖊^2")), typeof(u"Eₕ*a₀^3/𝖊^2")}
+typealias EnergyDensityDerivatives{Nrho, Ngrho, T <: CReal}
+    typeof(EnergyDensity(1)/Density(1)^(Nrho+Ngrho)*(1u"a₀")^Ngrho)
 
 typealias DensityArray{T <: CReal} DenseArray{Density, N}
 typealias EnergyDensityArray{T <: CReal, N} DenseArray{EnergyDensity, N}
 typealias PotentialDensityArray{T <: CReal, N} DenseArray{PotentialDensity, N}
+
+Base.one{T}(::Type{Density{T}}) = Density(one(T))
