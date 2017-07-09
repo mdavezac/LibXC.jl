f1be14dc06f736ee2fea742ff6353650a77bf3cf
Mayeul d'Avezac <mdavezac@gmail.com>
2017-07-09 19:23:13 +0100

    Macro hygiene problem

--- src/checks.jl
+++ src/checks.jl
@@ -2,7 +2,7 @@
 macro check_functional(funcname, functype)
     msg = "Incorrect number of arguments: input is not an $functype functional"
     quote
-        family($funcname) ≠ Constants.$functype && throw(ArgumentError($msg))
+        family($(esc(funcname))) ≠ Constants.$functype && throw(ArgumentError($msg))
     end
 end
 
@@ -10,14 +10,14 @@ end
 macro check_availability(funcname, functype)
     msg = "Functional does not implement energy."
     quote
-        Constants.$functype ∉ flags($funcname) && error($msg)
+        Constants.$functype ∉ flags($(esc(funcname))) && error($msg)
     end
 end
 """ Adds argument check for size compatibility """
 macro check_size(funcname, rhoname, outname, factor)
     msg = "sizes of $rhoname and $outname are incompatible"
     quote
-        if size($outname) ≠ output_size($funcname, $rhoname, $factor)
+        if size($(esc(outname))) ≠ output_size($(esc(funcname)), $(esc(rhoname)), $factor)
             throw(ArgumentError($msg))
         end
     end
